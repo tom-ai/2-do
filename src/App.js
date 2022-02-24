1ddcb394754bd4d91ad76d907520fbd4248e8845
Tom Harris <tomharris3@me.com>
2022-02-24 14:43:12 +0000

    adding components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-      <p>Hello</p>
-
-      </header>
+    <div className="body">
+      <Header />
+      <container className="list-container">
+        <PendingList />
+        <CompletedList />
+      </container>
     </div>
   );
 }
 
 export default App;
+
+const Header = () => {
+  return (
+    <div>
+      <h1>2-Do</h1>
+      <p>A simple to-do app</p>
+    </div>
+  )
+}
+
+const PendingList = () => {
+  return (
+    <div className='list-div'>
+      <h3>2-Do's</h3>
+
+    </div>
+  )
+}
+
+const CompletedList = () => {
+  return (
+    <div className='list-div'>
+      <h3>Completed 2-Do's</h3>
+    </div>
+  )
+}
+
+const input = () => {
+  return (
+    
+  )
+}
